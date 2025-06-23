e8c5fc338f13b4a413c1bd3a1f7ff7f6dc69f6ed
WaithakaGuru <waithakaoffices@gmail.com>
2025-06-23 19:25:53 +0300

    Add a logo to the project and the stylings for the logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export function Header() {
     return(
         <header>
-            <h1>Thought<span>Ful</span></h1>
+            <h1><img src="/thoughtful2.png" alt="logo" className="logo"/> Thought<span>Ful</span></h1>
             <p>Your Favourite Advice Habour</p>
         </header>
     )
